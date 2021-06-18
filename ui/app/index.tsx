bc7d41dba8b82e03da0ca58b684ac01beafc040f
wisepythagoras <55108282+wisepythagoras@users.noreply.github.com>
2021-06-18 10:27:44 -0400

    Add styled and material ui

--- ui/app/index.tsx
+++ ui/app/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Button } from '@material-ui/core';
 
 const Hello = (props: any) => {
     return (
-        <h1>
-            Hello, world!
-        </h1>
+        <div>
+            <h1>
+                Hello, world!
+            </h1>
+            <Button variant="contained" color="primary">Hello</Button>
+        </div>
     );
 };
 
