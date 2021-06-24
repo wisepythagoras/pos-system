3388d33ad97fc6427030ac55ef5f95888c43b717
wisepythagoras <55108282+wisepythagoras@users.noreply.github.com>
2021-06-24 16:58:29 -0400

    Add date to admin orders

--- ui/admin/components/RichOrder/index.tsx
+++ ui/admin/components/RichOrder/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Card, CardContent } from '@material-ui/core';
+import dayjs from 'dayjs';
 import { OrderProducts } from './OrderProducts';
 import { RichOrderT } from '../../../app/types';
 
 const GridContents = styled.div`
     display: grid;
-    grid-template-columns: 75px auto 75px;
+    grid-template-columns: 75px auto 75px 150px;
 
     & > div:first-child,
     & > div:last-child {
@@ -34,6 +35,9 @@ export const RichOrder = (props: IRichOrderProps) => {
                         <OrderProducts products={order.order.products} />
                     </div>
                     <div>${order.total}</div>
+                    <div>
+                        {dayjs(order.order.created_at).format('HH:mm MM-DD-YYYY')}
+                    </div>
                 </GridContents>
             </CardContent>
         </Card>
