6b7e06a901fffc0f9a2ed7bc12f04aba979a086a
wisepythagoras <55108282+wisepythagoras@users.noreply.github.com>
2021-07-02 10:14:46 -0400

    Add tabs

--- ui/admin/components/Main/index.tsx
+++ ui/admin/components/Main/index.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {
+    AppBar,
     Button,
     CircularProgress,
     Container,
+    Tab,
+    Tabs,
     Typography,
 } from '@material-ui/core';
 import { RichOrder } from '../RichOrder';
@@ -90,14 +93,22 @@ const useGetTotalEarnings = () => {
 
 export interface IMainProps {};
 
+interface IMainState {
+    page: number;
+    tab: number;
+}
+
 /**
  * Renders the main page.
  * @param props The props.
  * @returns The Main page component.
  */
 export const Main = (props: IMainProps) => {
-    const [page, setPage] = useState(1);
-    const { loading, error, orders, fetchOrders } = useGetOrdersList(page);
+    const [state, setState] = useState<IMainState>({
+        page: 1,
+        tab: 0,
+    });
+    const { loading, error, orders, fetchOrders } = useGetOrdersList(state.page);
     const earnings = useGetTotalEarnings();
 
     const exportTotals = () => {
@@ -108,43 +119,59 @@ export const Main = (props: IMainProps) => {
     };
 
     return (
-        <Container>
+        <div>
+            <AppBar>
+                <Tabs value={state.tab} onChange={(_, tab) => setState({ ...state, tab })}>
+                    <Tab label="Orders" />
+                    <Tab label="Users" />
+                </Tabs>
+            </AppBar>
             <br />
-            <Typography variant="h3" component="h3" gutterBottom>
-                Total Earnings: ${earnings.toFixed(2)}
-            </Typography>
-            <div style={{ marginBottom: '15px' }}>
-                <Button onClick={exportTotals} variant="contained" color="primary">
-                    Export
-                </Button>
-            </div>
-            <div>
-                {error ? (
-                    <Typography variant="h4" component="h4">
-                        {error}
+            <br />
+            {state.tab === 0 ? (
+                <Container>
+                    <br />
+                    <Typography variant="h3" component="h3" gutterBottom>
+                        Total Earnings: ${earnings.toFixed(2)}
                     </Typography>
-                ) : null}
-
-                {loading ? (
-                    <div style={{ textAlign: 'center' }}>
-                        <CircularProgress
-                            variant="indeterminate"
-                            disableShrink
-                            style={{
-                                strokeLinecap: 'round',
-                                color: '#1a90ff',
-                                animationDuration: '550ms',
-                                marginTop: '20px',
-                            }}
-                            size={40}
-                            thickness={4}
-                        />
+                    <div style={{ marginBottom: '15px' }}>
+                        <Button onClick={exportTotals} variant="contained" color="primary">
+                            Export
+                        </Button>
                     </div>
-                ) : null}
+                    <div>
+                        {error ? (
+                            <Typography variant="h4" component="h4">
+                                {error}
+                            </Typography>
+                        ) : null}
 
-                {orders.map((order) => <RichOrder order={order} />)}
-            </div>
-            <br />
-        </Container>
+                        {loading ? (
+                            <div style={{ textAlign: 'center' }}>
+                                <CircularProgress
+                                    variant="indeterminate"
+                                    disableShrink
+                                    style={{
+                                        strokeLinecap: 'round',
+                                        color: '#1a90ff',
+                                        animationDuration: '550ms',
+                                        marginTop: '20px',
+                                    }}
+                                    size={40}
+                                    thickness={4}
+                                />
+                            </div>
+                        ) : null}
+
+                        {orders.map((order) => <RichOrder order={order} />)}
+                    </div>
+                    <br />
+                </Container>
+            ) : null}
+
+            {state.tab === 1 ? (
+                <div>Hello</div>
+            ) : null}
+        </div>
     );
 };
